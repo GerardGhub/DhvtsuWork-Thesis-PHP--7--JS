2211fc0fada921a72aa3a92e62c452df8324af28
gerard singian <gerard.singian@outlook.com>
2022-01-18 22:24:06 +0800

    Teacher Validations

---
+++ view/Teacher/adddepartmentsection.php
@@ -0,0 +1,61 @@
+<?php
+
+
+
+$dbcon=mysqli_connect("127.0.0.1","root","");
+
+mysqli_select_db($dbcon,"std_db");
+
+
+
+?>
+
+<?php
+if(isset($_POST['item_save']))
+
+{
+$classname = $_POST['classname'];
+//$blink = $_POST['blink'];
+
+ 
+ $check_item="select category_id from misdepfinal WHERE category_id='category_id'";
+    $run_query=mysqli_query($dbcon,$check_item);
+
+
+
+		
+
+				
+	
+		
+
+	
+					$saveitem="insert into misdepfinal (classname,blink,date_added) VALUE ('$classname','1',CURDATE())";
+					mysqli_query($dbcon,$saveitem);
+					 echo "<script>alert('Data successfully saved!')</script>";				
+					 echo "<script>window.open('managesection.php','_self')</script>";
+				}
+				else{
+					
+					
+				}
+		
+
+				
+				   
+				
+	
+		
+	
+		
+
+
+?>
+
+
+      <!--<a data-toggle="modal" data-target="#uploadModalcategorymis"><i class="fa fa-plus-circle">-->
+
+
+
+
+
